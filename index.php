7a027ae8961b4e1de376cd2d840bdca6f743e8c7
Phithounsavanh <Phithounsavanh@gmail.com>
2019-04-30 08:33:30 -0400

    PDO select from a table and return back in array

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+  //hostname
+  $host = "localhost";
+  //username used to access to the database
+  $user = 'root';
+  //password used to access to the database
+  $password = '123456';
+  //the database you want to access
+  $dbname = 'podtest';
+
+  //Set DSN
+  $dsn = 'mysql:host='. $host .';dbname='.$dbname;
+  //Create a PDO instance for establishing a database's connection
+  $pdo = new PDO($dsn, $user, $password);
+
+  $status = 'admin';
+  //Create a SQL command (:status is a name paramater)
+  $sql = 'SELECT * FROM users WHERE status = :status';
+  $statement = $pdo->prepare($sql);
+  $statement->execute(['status'=>$status]);
+  //get all data from the return
+  $users = $statement->fetchAll();
+  //show data by loop
+  foreach($users as $user){
+    echo $user['name'].'<br>';
+  }
+  
